852c384bf1b4bfc8fd46ec10b05e3755b59d5c96
David Iacovone <diacovone@fandom.com>
2024-12-04 11:19:44 +0100

    COTECH-1614 updated evl player script

--- js/resize.js
+++
@@ -1,8 +0,0 @@
-(function(mw, $, window) {
-	$(function() {
-		$('.embedvideo-evlbox').css('display', 'flex');
-		window?.autoResizer?.();
-		window.addEventListener('FandomDesktopContentSize', () => window?.autoResizer?.());
-	});
-
-}(mediaWiki, jQuery, window));
